46c7a17847a919439d0a961407e2de45b8fd9ce8
Asatogenki <asa.326.gen@gmail.com>
2020-06-25 23:54:35 +0900

    lこみっと

--- selection.rb
+++ selection.rb
@@ -1,39 +1,39 @@
-# require 'benchmark'
-# require './selection/bubbleSearchFunc'
-# require './selection/selectionSortFunc'
+require 'benchmark'
+require './selection/bubbleSearchFunc'
+require './selection/selectionSortFunc'
 
 
-# class Selection
+class Selection
   
 
-#   include BSF
-#   include SSF
+  include BSF
+  include SSF
 
-#   def Ssf(n)
-#     p ssfAsc(n)
-#     p ssfDesc(n)
-#     puts "//////////////////////////"
-#     return "選択ソートです"
-#   end
+  def Ssf(n)
+    p ssfAsc(n)
+    p ssfDesc(n)
+    puts "//////////////////////////"
+    return "選択ソートです"
+  end
 
-#   def Bsf(n,y)
-#     if y == 1
-#       p bsfAsc(n)
-#       p bsfDesc(n)
-#       puts "//////////////////////////"
-#       puts "バブルソートです"
-#     else
-#       p bsfAsc_for(n)
-#       p bsfDesc_for(n)
-#       puts "//////////////////////////"
-#       puts"for文を使ったバブルソートです"
-#       return 
-#     end
-#   end
-# end
+  def Bsf(n,y)
+    if y == 1
+      p bsfAsc(n)
+      p bsfDesc(n)
+      puts "//////////////////////////"
+      puts "バブルソートです"
+    else
+      p bsfAsc_for(n)
+      p bsfDesc_for(n)
+      puts "//////////////////////////"
+      puts"for文を使ったバブルソートです"
+      return 
+    end
+  end
+end
 
-# ns = (1..100).to_a.shuffle
-# selection = Selection.new
+ns = (1..100).to_a.shuffle
+selection = Selection.new
 
 
-# selection.Bsf(ns,0)
+selection.Bsf(ns,0)
